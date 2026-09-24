6635204fb6c214b797c590408bff7ce007caf1c9
AnnaButova <anya.butova2017@gmail.com>
2026-09-23 22:12:08 -0400

    now able to read json files

--- AnnaButova_TP1.py
+++ AnnaButova_TP1.py
@@ -1,1 +1,15 @@
+import PySide6
+import os # importer la librairie pour communiquer avec le système d'exploitation
+import json
+
 print("Yo! I work!")
+
+if os.path.exists("data_small.json"): # with os check if json file exists, show message accordingly
+    print("The file exists.") 
+else:
+    print("The file does not exist.")
+
+with open("data_small.json", "r", -1, "UTF-8") as json_file: # opens the file
+    data = json.load(json_file) # converts file to .json format
+
+print(data[0]) # prints the first "block" of the json file
