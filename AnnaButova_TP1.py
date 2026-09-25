6c2034d2f47f7dd28ffc2ba4c62e19b7abcb689b
AnnaButova <anya.butova2017@gmail.com>
2026-09-25 17:03:54 -0400

    added table

--- AnnaButova_TP1.py
+++ AnnaButova_TP1.py
@@ -1,15 +1,61 @@
-import PySide6
-import os # importer la librairie pour communiquer avec le système d'exploitation
+import os
 import json
+import sys
+from pathlib import Path
 
-print("Yo! I work!")
+# Imports for UI
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTableWidget,
+    QTableWidgetItem
+) 
 
-if os.path.exists("data_small.json"): # with os check if json file exists, show message accordingly
-    print("The file exists.") 
+# Find the json files ----------------------
+path_small = Path("data_small.json")
+path_large = Path("data_large.json")
+
+if path_small.exists() or path_large.exists():
+    print("I HAVE BEEN FOUND")
 else:
-    print("The file does not exist.")
+    print("LOL NOPE")
+# ------------------------------------------
 
-with open("data_small.json", "r", -1, "UTF-8") as json_file: # opens the file
-    data = json.load(json_file) # converts file to .json format
+# Open json file ---------------
+with open("data_small.json", "r", -1, "utf-8") as json_file_small:
+    data_small = json.load(json_file_small)
+# ------------------------------------------
 
-print(data[0]) # prints the first "block" of the json file
+for i in data_small:
+    for key, value in i.items():
+        print(key, value)
+
+
+app = QApplication([])
+window = QMainWindow()
+
+table = QTableWidget()
+table.setRowCount(len(data_small))
+table.setColumnCount(6)
+table.setHorizontalHeaderLabels(["Id", "Nom", "Categorie", "Format", "Polygones", "Statut"])
+
+for i in range(len(data_small)):
+    item = data_small[i]
+    table.setItem(i, 0, QTableWidgetItem(item["id"])) # shows json elements in the row "id"
+    table.setItem(i, 1, QTableWidgetItem(item["nom"]))
+    table.setItem(i, 2, QTableWidgetItem(item["categorie"]))
+    table.setItem(i, 3, QTableWidgetItem(item["format"]))
+    table.setItem(i, 4, QTableWidgetItem(str(item["polygones"]))) # shows polygons (int) as a string (str)
+    table.setItem(i, 5, QTableWidgetItem(item["statut"]))
+
+table.show()
+sys.exit(app.exec())
+
+'''
+for i in data_small:
+    for key in i.keys():
+        print (key)
+        
+    for value in i.values():
+        print(value)
+'''
